f2b9a3ddf0c9c940c8eb367065445a5b8fe7cde8
Randy <randyisbusy@gmail.com>
2020-03-08 05:40:17 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function Scooter(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model
+} 
+
+function Driver(name, age, ex){
+    this.name = name;
+    this.age = age;
+    this.experience = ex 
+} 
+function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+}
